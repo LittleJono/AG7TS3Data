b35ad66a03a80bdbe44e4714bdb6efc6fd99b252
Jono Tinfoil <Jono Tinfoil>
2017-04-06 20:06:55 +1200

    update

--- getdata.js
+++ getdata.js
@@ -3,6 +3,8 @@ var redis = new Redis({db: 0});
 // Create a readable stream (object mode)
 var stream = redis.scanStream();
 var keys = [];
+var fs = require('fs');
+
 stream.on('data', function (resultKeys) {
   // `resultKeys` is an array of strings representing key names
   for (var i = 0; i < resultKeys.length; i++) {
@@ -10,5 +12,13 @@ stream.on('data', function (resultKeys) {
   }
 });
 stream.on('end', function () {
-  console.log('done with the keys: ', keys);
+    //console.log('done with the keys: ', keys.sort());
+    keys = keys.sort();
+    for (i in keys) {
+        redis.get(keys[i]).then(function (result) {
+            fs.appendFile('data.txt', result + "\r\n", function (err) {
+            if (err) throw err;    
+            });
+        });
+    }
 });
